3e78e3fe45c1d6384a6493766a6591e4859c45d6
maotterson <otterson.2@gmail.com>
2023-05-03 01:37:30 -0400

    Add project files.

---
+++ GeoMastery.Domain/Models/City.cs
@@ -0,0 +1,8 @@
+﻿namespace GeoMastery.Domain.Models;
+public class City
+{
+    public int Id { get; set; }
+    public Country Country { get; set; }
+    public string Name { get; set; }
+    public bool IsCapital { get; set; }
+}
---
+++ GeoMastery.Domain/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace GeoMastery.Domain.Models;
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public City Capital { get; set; }
+    public List<City> OtherCities { get; set; }
+}
