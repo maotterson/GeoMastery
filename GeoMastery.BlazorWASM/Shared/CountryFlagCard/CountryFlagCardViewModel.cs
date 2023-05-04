fefcf32ff9cf47ba6dac525e98b51d40b906532d
maotterson <otterson.2@gmail.com>
2023-05-04 02:05:35 -0400

    add json serialization for flag_base64 field in flagcardviewmodel

--- GeoMastery.BlazorWASM/Shared/CountryFlagCard/CountryFlagCardViewModel.cs
+++ GeoMastery.BlazorWASM/Shared/CountryFlagCard/CountryFlagCardViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace GeoMastery.BlazorWASM.Shared.CountryFlagCard;
+﻿using System.Text.Json.Serialization;
 
+namespace GeoMastery.BlazorWASM.Shared.CountryFlagCard;
 public class CountryFlagCardViewModel
 {
     public string Country { get; set; }
+
+    [JsonPropertyName("flag_base64")]
     public string FlagBase64 { get; set; }
 }
