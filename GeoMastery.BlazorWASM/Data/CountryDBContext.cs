c21a284aaccbe8e4377a1d1ca6cb0d74ffeec9c3
maotterson <otterson.2@gmail.com>
2023-05-05 01:21:07 -0400

    install ef core + ef core sqlite library, add dbcontext

---
+++ GeoMastery.BlazorWASM/Data/CountryDBContext.cs
@@ -0,0 +1,19 @@
+﻿using GeoMastery.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoMastery.BlazorWASM.Data;
+
+public class CountryDbContext : DbContext
+{
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=countries.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Country>().HasKey(c => c.Name);
+    }
+}
