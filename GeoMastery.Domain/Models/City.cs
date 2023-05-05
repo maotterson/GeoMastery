95c2a95331dd344b3ab0a10dcf541daef9561893
maotterson <otterson.2@gmail.com>
2023-05-05 01:42:34 -0400

    update city + country entities for use with EF Core

--- GeoMastery.Domain/Models/City.cs
+++ GeoMastery.Domain/Models/City.cs
@@ -2,7 +2,7 @@
 public class City
 {
     public int Id { get; set; }
+    public int CountryId { get; set; }
     public Country Country { get; set; }
     public string Name { get; set; }
-    public bool IsCapital { get; set; }
 }
--- GeoMastery.Domain/Models/Country.cs
+++ GeoMastery.Domain/Models/Country.cs
@@ -4,6 +4,6 @@ public class Country
     public int Id { get; set; }
     public string Name { get; set; }
     public string Code { get; set; }
-    public City Capital { get; set; }
-    public List<City> OtherCities { get; set; }
+    public int CapitalId { get; set; }
+    public List<City> Cities { get; set; }
 }
