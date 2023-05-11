6de4629dd50059784036b49a22c056ae21ab0697
maotterson <otterson.2@gmail.com>
2023-05-11 01:06:03 -0400

    create continent controller boilerplate

---
+++ GeoMastery.CountriesAPI/Controllers/v1/ContinentController.cs
@@ -0,0 +1,27 @@
+﻿using GeoMastery.BlazorWASM.Data;
+using GeoMastery.CountriesAPI.Dto.v1;
+using GeoMastery.CountriesAPI.Extensions.v1;
+using GeoMastery.CountriesAPI.Services.v1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoMastery.CountriesAPI.Controllers.v1;
+[ApiVersion("1.0")]
+[Route("api/v1/[controller]")]
+[ApiController]
+public class ContinentController : ControllerBase
+{
+    private readonly IContinentService _continentService;
+
+    public ContinentController(CountryDbContext context, IContinentService continentService)
+    {
+        _continentService = continentService;
+    }
+
+    // GET: api/v1/continents/
+    [HttpGet("")]
+    public async Task<ActionResult<IEnumerable<ContinentDto>>> GetAllContinents()
+    {
+        var continents = await _continentService.GetAllContinentsAsync();
+        return Ok(continents.ToDto());
+    }
+}
