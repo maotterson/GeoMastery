52b53dc2c3b529cc9761bc4ed68652bc7e2bc0b8
maotterson <otterson.2@gmail.com>
2023-05-12 00:40:34 -0400

    create country summar card

---
+++ GeoMastery.BlazorWASM/Shared/CountrySummaryCard/CountrySummaryCardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GeoMastery.BlazorWASM.Shared.CountrySummaryCard;
+public class CountrySummaryCardViewModel
+{
+    public string Country { get; set; }
+    public string Capital { get; set; }
+
+    [JsonPropertyName("flag_base64")]
+    public string FlagBase64 { get; set; }
+}
