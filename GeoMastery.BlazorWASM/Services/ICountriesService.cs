8addc3179cacc522fe91a9862aa22ddd5410e9da
maotterson <otterson.2@gmail.com>
2023-05-12 01:30:27 -0400

    inject service into region to grab dto for the page by slug

--- GeoMastery.BlazorWASM/Services/ICountriesService.cs
+++ GeoMastery.BlazorWASM/Services/ICountriesService.cs
@@ -1,5 +1,8 @@
-﻿namespace GeoMastery.BlazorWASM.Services;
+﻿using GeoMastery.CountriesAPI.Contracts.Dto.v1;
+
+namespace GeoMastery.BlazorWASM.Services;
 
 public interface ICountriesService
 {
+    Task<RegionDto> GetRegionBySlugAsync(string slug);
 }
