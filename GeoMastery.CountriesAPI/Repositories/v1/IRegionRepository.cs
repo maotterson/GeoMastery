d4f5e059fa67698c7af61ecd085b1eb6770dc1fa
maotterson <otterson.2@gmail.com>
2023-05-12 00:02:21 -0400

    implement getregionbyslug (previous commit was continent)

--- GeoMastery.CountriesAPI/Repositories/v1/IRegionRepository.cs
+++ GeoMastery.CountriesAPI/Repositories/v1/IRegionRepository.cs
@@ -5,4 +5,5 @@ namespace GeoMastery.CountriesAPI.Repositories.v1;
 public interface IRegionRepository
 {
     Task<List<Region>> GetAllRegionsAsync();
+    Task<Region> GetRegionBySlugAsync(string slug);
 }
