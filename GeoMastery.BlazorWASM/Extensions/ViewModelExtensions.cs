d3ac6cc0e6e9b949d3318c245a7d080085ac0547
maotterson <otterson.2@gmail.com>
2023-05-12 00:26:58 -0400

    implement continent card list on continents page + create vm extension mapper

---
+++ GeoMastery.BlazorWASM/Extensions/ViewModelExtensions.cs
@@ -0,0 +1,22 @@
+﻿using GeoMastery.BlazorWASM.Shared.ContinentCard;
+using GeoMastery.CountriesAPI.Contracts.Dto.v1;
+using GeoMastery.Domain.Models;
+
+namespace GeoMastery.CountriesAPI.Extensions.v1;
+
+public static class ViewModelExtensions
+{
+    public static ContinentCardViewModel ToViewModel(this ContinentDto continent)
+    {
+        return new ContinentCardViewModel
+        {
+            Name = continent.Name,
+            Slug = continent.Slug
+        };
+    }
+
+    public static List<ContinentCardViewModel> ToDto(this List<ContinentDto> continents)
+    {
+        return continents.Select(c => c.ToViewModel()).ToList();
+    }
+}
