382e71fbd172670be5e73f2ca12d276615c5cf56
maotterson <otterson.2@gmail.com>
2023-05-12 00:48:57 -0400

    add country summary card to continent page + extension method

--- GeoMastery.BlazorWASM/Extensions/ViewModelExtensions.cs
+++ GeoMastery.BlazorWASM/Extensions/ViewModelExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using GeoMastery.BlazorWASM.Shared.ContinentCard;
+using GeoMastery.BlazorWASM.Shared.CountrySummaryCard;
 using GeoMastery.BlazorWASM.Shared.RegionCard;
 using GeoMastery.CountriesAPI.Contracts.Dto.v1;
 
@@ -28,9 +29,25 @@ public static class ViewModelExtensions
             Slug = region.Slug
         };
     }
-
     public static List<RegionCardViewModel> ToDto(this List<RegionDto> regions)
     {
         return regions.Select(c => c.ToViewModel()).ToList();
+    }
+
+    public static CountrySummaryCardViewModel ToSummaryCardViewModel(this CountryDto country)
+    {
+        return new CountrySummaryCardViewModel
+        {
+            Country = country.Country,
+            Slug = country.Slug,
+            Continent = country.Continent,
+            FlagBase64 = country.FlagBase64,
+            Capital = country.Capital,
+            Region = country.Region
+        };
+    }
+    public static List<CountrySummaryCardViewModel> ToSummaryCardViewModel(this List<CountryDto> countries)
+    {
+        return countries.Select(c => c.ToSummaryCardViewModel()).ToList();
     }
 }
--- GeoMastery.BlazorWASM/Shared/CountrySummaryCard/CountrySummaryCardViewModel.cs
+++ GeoMastery.BlazorWASM/Shared/CountrySummaryCard/CountrySummaryCardViewModel.cs
@@ -3,9 +3,12 @@
 namespace GeoMastery.BlazorWASM.Shared.CountrySummaryCard;
 public class CountrySummaryCardViewModel
 {
-    public string Country { get; set; }
-    public string Capital { get; set; }
+    public string Country { get; set; } = default!;
+    public string? Capital { get; set; }
+    public string Slug { get; set; } = default!;
+    public string Region { get; set; } = default!;
+    public string Continent { get; set; } = default!;
 
     [JsonPropertyName("flag_base64")]
-    public string FlagBase64 { get; set; }
+    public string? FlagBase64 { get; set; }
 }
