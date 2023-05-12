5ba6bfde6e9ef771dfd037f991b9e2720c690953
maotterson <otterson.2@gmail.com>
2023-05-12 01:47:53 -0400

    create back button component + state pattern to track history

--- GeoMastery.BlazorWASM/Program.cs
+++ GeoMastery.BlazorWASM/Program.cs
@@ -1,5 +1,6 @@
 using GeoMastery.BlazorWASM;
 using GeoMastery.BlazorWASM.Services;
+using GeoMastery.BlazorWASM.State;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ICountriesService, CountriesService>();
+builder.Services.AddSingleton<PageHistoryState>();
 
 
 // Build the hostS
