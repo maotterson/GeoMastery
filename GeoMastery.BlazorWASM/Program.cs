01631e9cf72efb5c367f4f924464535ea2f272c2
maotterson <otterson.2@gmail.com>
2023-05-14 23:20:52 -0400

    swap page history state manager for js interop back functionality

--- GeoMastery.BlazorWASM/Program.cs
+++ GeoMastery.BlazorWASM/Program.cs
@@ -1,6 +1,7 @@
 using GeoMastery.BlazorWASM;
 using GeoMastery.BlazorWASM.Services;
-using GeoMastery.BlazorWASM.State;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,9 +12,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ICountriesService, CountriesService>();
-builder.Services.AddSingleton<PageHistoryState>();
-
 
 // Build the hostS
 var host = builder.Build();
+
 await host.RunAsync();
+
