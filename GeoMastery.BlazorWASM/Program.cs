c8409b2c18818ef072e9b1ab90719c50ed4732d7
maotterson <otterson.2@gmail.com>
2023-05-14 23:44:51 -0400

    register service/repository layers from persistence project in wasm

--- GeoMastery.BlazorWASM/Program.cs
+++ GeoMastery.BlazorWASM/Program.cs
@@ -1,5 +1,7 @@
 using GeoMastery.BlazorWASM;
 using GeoMastery.BlazorWASM.Services;
+using GeoMastery.Persistence.Repositories.v1;
+using GeoMastery.Persistence.Services.v1;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.Web;
@@ -11,9 +13,16 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ICountriesService, CountriesService>();
 
-// Build the hostS
+// Register persistence/application layers
+builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<IContinentService, ContinentService>();
+builder.Services.AddScoped<IContinentRepository, ContinentRepository>();
+builder.Services.AddScoped<IRegionService, RegionService>();
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+
+// Build the host
 var host = builder.Build();
 
 await host.RunAsync();
--- GeoMastery.BlazorWASM/Services/ICountriesService.cs
+++
@@ -1,8 +0,0 @@
-﻿using GeoMastery.CountriesAPI.Contracts.Dto.v1;
-
-namespace GeoMastery.BlazorWASM.Services;
-
-public interface ICountriesService
-{
-    Task<RegionDto> GetRegionBySlugAsync(string slug);
-}
