76dea62e2b70a0ca2e1aa8c76d1c604170dd9fe7
maotterson <otterson.2@gmail.com>
2023-05-15 23:36:35 -0400

    add dto-> entity mapping implementations

--- GeoMastery.BlazorWASM/Extensions/DtoExtensions.cs
+++ GeoMastery.BlazorWASM/Extensions/DtoExtensions.cs
@@ -5,11 +5,11 @@ namespace GeoMastery.BlazorWASM.Extensions;
 
 public static class DtoExtensions
 {
-    public static Country ToCountry(CountryDto countryDto)
+    public static Country ToCountry(this CountryDto countryDto)
     {
         return new Country
         {
-            Id = Guid.NewGuid(),
+            Id = countryDto.Id,
             Name = countryDto.Country,
             Code = countryDto.Code,
             Slug = countryDto.Slug,
@@ -19,5 +19,35 @@ public static class DtoExtensions
             ContinentId = countryDto.ContinentId,
             RegionId = countryDto.RegionId,
         };
+    }
+    public static List<Country> ToCountries(this List<CountryDto> countryDtos)
+    {
+        return countryDtos.Select(c => c.ToCountry()).ToList();
+    }
+    public static Continent ToContinent(this ContinentDto continentDto)
+    {
+        return new Continent
+        {
+            Id = continentDto.Id,
+            Name = continentDto.Name,
+            Slug = continentDto.Slug
+        };
+    }
+    public static List<Continent> ToContinents(this List<ContinentDto> continentDtos)
+    {
+        return continentDtos.Select(c => c.ToContinent()).ToList();
+    }
+    public static Region ToRegion(this RegionDto regionDto)
+    {
+        return new Region
+        {
+            Id = regionDto.Id,
+            Name = regionDto.Name,
+            Slug = regionDto.Slug
+        };
+    }
+    public static List<Region> ToRegions(this List<RegionDto> regionDtos)
+    {
+        return regionDtos.Select(r => r.ToRegion()).ToList();
     }
 }
