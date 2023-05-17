72c19836e1a060f9be82982d87f515f7f1c694d9
maotterson <otterson.2@gmail.com>
2023-05-17 01:03:00 -0400

    create capitallocalrepository to persistcities

---
+++ GeoMastery.BlazorWASM/Repositories/Local/CapitalLocalRepository.cs
@@ -0,0 +1,21 @@
+﻿using GeoMastery.Domain.Models;
+using GeoMastery.Persistence.Data;
+using SqliteWasmHelper;
+
+namespace GeoMastery.BlazorWASM.Repositories.Local;
+
+public class CapitalLocalRepository
+{
+    private readonly ISqliteWasmDbContextFactory<CountryDbContext> _factory;
+    public CapitalLocalRepository(ISqliteWasmDbContextFactory<CountryDbContext> factory)
+    {
+        _factory = factory;
+    }
+
+    public async Task AddAsync(City capital)
+    {
+        using var ctx = await _factory.CreateDbContextAsync();
+        await ctx.Cities.AddAsync(capital);
+        await ctx.SaveChangesAsync();
+    }
+}
